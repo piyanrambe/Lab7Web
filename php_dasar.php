44d5d1bc2f90bbf4cdec4a15dd0d3096275b017b
piyanrambe <alfiansyahrambe@mhs.pelitabangsa.ac.id>
2022-05-17 22:41:28 +0700

    menambah file

---
+++ php_dasar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Dasar</title>
+</head>
+
+<body>
+  <h1>Belajar PHP Dasar</h1>
+  <?php
+  echo "Hello World";
+  ?><br>
+
+  <!-- Variabel PHP -->
+  <h1>Variabel PHP</h1>
+  <?php
+  $nim = "312010338";
+  $nama = 'Alfiansyah Rambe';
+  echo "NIM : " . $nim . "<br>";
+  echo "Nama : $nama";
+  ?><br>
+</body>
+
+</html>
